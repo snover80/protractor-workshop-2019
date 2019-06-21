7e5cf385b6d544341c048426d1a92d66a77ae66e
Cristian Velandia <cristian.velandia@e-hps.com>
2019-06-21 09:46:10 -0500

    Add static code analysis

--- protractor/headless.config.ts
+++ protractor/headless.config.ts
@@ -4,7 +4,7 @@ import { reporter } from './helpers/reporter';
 export const config: Config = {
   framework: 'jasmine',
   SELENIUM_PROMISE_MANAGER: false,
-  specs: [ '../test/google.spec.js' ],
+  specs: ['../test/google.spec.js'],
   capabilities: {
     browserName: 'chrome',
     chromeOptions: {
